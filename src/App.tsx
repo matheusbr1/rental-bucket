18498a43c403e2f5416413381d1705d2788bb00f
Matheus <matheusbaron10@gmail.com>
2021-03-28 14:13:19 -0300

    Feat: route system added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import Routes from './routes'
 
 function App() {
   return (
-   <h1>Hello new rental bucket</h1>
-  );
+    <Routes />
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <h1>Sign In page</h1>
+    </Container>
+  )
+}
+
+export default SignIn
