3f47218a15efab0bb686e461650655c1368117ad
Matheus Baron <matheusbaron10@gmail.com>
2022-06-18 18:22:31 -0300

    Fix: refresh token req fail

--- src/hooks/usePrivateApi.ts
+++ src/hooks/usePrivateApi.ts
@@ -1,26 +1,44 @@
 import { useCallback, useEffect } from "react"
 import { privateApi } from "services/api"
 import usePersistedState from "./usePersistedState"
+import { useHistory } from 'react-router-dom'
+import { signOut } from "redux/user/user.actions"
+import { useSnackbar } from "notistack"
+import { useDispatch } from "react-redux"
 
 const usePrivateApi = () => {
   const [tokens, setTokens] = usePersistedState('@rentalbucket:tokens', null)
 
+  const { enqueueSnackbar: snackbar } = useSnackbar()
+
+  const dispatch = useDispatch()
+
+  const history = useHistory()
+
   const refresh = useCallback(async () => {
-    const { data } = await privateApi.post('/refresh-token', {
-      token: tokens?.refreshToken
-    })
-
-    const updatedTokens = {
-      accessToken: data.token as string,
-      refreshToken: data.refresh_token as string
-    }
-
-    // console.log(tokens, updatedTokens)
-
-    setTokens(updatedTokens)
+    try {
+      const { data } = await privateApi.post('/refresh-token', {
+        token: tokens?.refreshToken
+      })
   
-    return data.token
-  }, [setTokens, tokens])
+      const updatedTokens = {
+        accessToken: data.token as string,
+        refreshToken: data.refresh_token as string
+      }
+  
+      // console.log(tokens, updatedTokens)
+  
+      setTokens(updatedTokens)
+    
+      return data.token
+    } catch (err) {
+      dispatch(signOut())
+
+      snackbar('Sessão expirada, faça login novamente', { variant: 'info' })
+
+      history.push('/')
+    }
+  }, [dispatch, history, setTokens, snackbar, tokens?.refreshToken])
 
   useEffect(() => {
     const requestIntercept = privateApi.interceptors.request.use(
